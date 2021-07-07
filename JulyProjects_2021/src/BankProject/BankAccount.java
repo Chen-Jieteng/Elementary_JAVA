c60740fe63e09e43e29fea1f0ecf1d25f6f37a33
edwin9999 <j.chen1150964115@gmail.com>
2021-07-07 02:49:08 -0400

    July 7 2021

---
+++ JulyProjects_2021/src/BankProject/BankAccount.java
@@ -0,0 +1,13 @@
+package BankProject;
+
+public class BankAccount {
+	private double balance;
+	private int accountNumber;
+	private static int lastAssignedNumber = 1000;
+	// public static final double OVERDRFT_FEE = 29.95;
+	
+	public BankAccount() {
+		lastAssignedNumber++;
+		accountNumber = lastAssignedNumber;
+	}
+}
---
+++ JulyProjects_2021/src/BankProject/Financial.java
@@ -0,0 +1,15 @@
+package BankProject;
+
+public class Financial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double taxRate = 0.01;
+		double total = 100;
+		double tax = Financial.percentOf(taxRate, total);
+
+	}
+	public static double percentOf(double percentage, double amount) {
+		return (percentage / 100) * amount;
+	}
+}
